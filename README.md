4d83b3a765b95b6429c9792c496fcb33aad62262
MrReSc <resc@mailbox.org>
2021-02-10 16:00:48 +0100

    timer basics implemented

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Headi
 Headache diary
+
+## Libraries used
+- [TimerX](https://github.com/arsvechkarev/TimerX) Apache-2.0 License
--- app/src/main/java/com/example/headi/ui/timer/TimerFragment.java
+++ app/src/main/java/com/example/headi/ui/timer/TimerFragment.java
@@ -1,9 +1,12 @@
 package com.example.headi.ui.timer;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,22 +17,57 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.headi.R;
 
+import java.util.concurrent.TimeUnit;
+
+import timerx.Stopwatch;
+import timerx.StopwatchBuilder;
+
 public class TimerFragment extends Fragment {
 
     private TimerViewModel timerViewModel;
+    private View view;
+    private Stopwatch stopwatch;
+    private Boolean startButtonClicked = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        timerViewModel =
-                new ViewModelProvider(this).get(TimerViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_timer, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        timerViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        Context context = getActivity();
+
+        timerViewModel = new ViewModelProvider(this).get(TimerViewModel.class);
+        view = inflater.inflate(R.layout.fragment_timer, container, false);
+
+        TextView mTextViewTimer = (TextView)view.findViewById(R.id.timer_time);
+        stopwatch = new StopwatchBuilder()
+                // Set the initial format
+                .startFormat("HH:MM:SS")
+                // Set the tick listener for displaying time
+                .onTick(time -> mTextViewTimer.setText(time))
+                // When time is equal to one hour, change format to "HH:MM:SS"
+                .changeFormatWhen(1, TimeUnit.HOURS, "HH:MM:SS")
+                .build();
+
+        registerListeners(context);
+
+        return view;
+    }
+
+    private void registerListeners(Context context) {
+        // Start / Stop Button listener
+        final Button button = view.findViewById(R.id.timer_startOrStop_button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                if (startButtonClicked) {
+                    stopwatch.stop();
+                    button.setText(context.getString(R.string.timer_start));
+                    startButtonClicked = false;
+                }
+                else {
+                    stopwatch.start();
+                    button.setText(context.getString(R.string.timer_stop));
+                    startButtonClicked = true;
+                }
             }
         });
-        return root;
     }
 }
